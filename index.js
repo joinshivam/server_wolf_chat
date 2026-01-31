f8863c0e45974730a235f6c2abb4bbde7d46547e
Shivam Kumar <lovekashyp4@gmail.com>
2026-01-31 11:02:15 +0530

    Change server listen address to 0.0.0.0

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
-// index.js
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const setupSocket = require("./socket");
 require("dotenv").config();
 const PORT = process.env.PORT || 5000;
-
 const app = express();
 const server = http.createServer(app);
-
 const io = new Server(server, {
   cors: { origin: true },
 });
-
 setupSocket(io);
-
-server.listen(PORT, () =>
+server.listen(PORT,"0.0.0.0", () =>
   console.log(`Global chat server running on PORT : ${PORT}`)
 );
+
 
